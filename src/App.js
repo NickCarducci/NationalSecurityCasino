ea3ad04c641be75db992cfa6b619f7372bf84396
Nick Carducci <nmcarducci@gmail.com>
2021-12-11 13:42:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -296,16 +296,26 @@ export default class App extends React.Component {
           But $3k/day/person is 2/3 private and velocity of m2 minus gdp divided
           by m2 minus CurrencyComponentOfM1 is 1.1/year, that is checking.
         </h2>
-        “There is never enough money to pay for theory, and Marxism is an
-        unachievable, lousy idea. Not reality, stealing from Hagel, Rousseau,
-        material-based, in a form of genocide and the latest American-form.
-        Inflation doesn’t come from matter of routine, but by not following the
-        laws of economics, that are like those of physics. If you don’t, things
-        fall. If you think about it, it is pretty simple. You can be a janitor
-        or slip and fall lawyer and understand that creating massive debt you
-        are unable to pay, with low interest rates, is so that consumer side nor
-        producer side notice. Now apparent with the highest inflation rate in 40
-        years, and it was created by Biden and his administration.”
+        Fiduciary Ryan Payne: "Bond buying by the Fed," is NOT ONLY third party
+        beneficiary donee claimable transaction, but outside of article 4 outlay
+        and receipt scope for share-split and free rider mutable tax and debt
+        servicing.
+        <br />
+        <br />
+        “There is never enough money to pay for theory, and&nbsp;
+        <a href="https://www.marxists.org/archive/marx/works/1848/communist-manifesto/ch02.htm">
+          Marxism
+        </a>
+        &nbps;is an unachievable, lousy idea. Not reality, stealing from Hagel,
+        Rousseau, material-based, in a form of genocide and the latest
+        American-form. Inflation doesn’t come from matter of routine, but by not
+        following the laws of economics, that are like those of physics. If you
+        don’t, things fall. If you think about it, it is pretty simple. You can
+        be a janitor or slip and fall lawyer and understand that creating
+        massive debt you are unable to pay, with low interest rates, is so that
+        consumer side nor producer side notice. Now apparent with the highest
+        inflation rate in 40 years, and it was created by Biden and his
+        administration.”
         <br />
         <br />
         "Marx predicted a period of despotism." That is the foreclosure induced
