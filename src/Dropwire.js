fd76e639a3993abc9c7d6a2458b0d3f3c3b0b3ec
Nick Carducci <nmcarducci@gmail.com>
2022-01-03 03:55:27 -0500

    Update Dropwire.js

--- src/Dropwire.js
+++ src/Dropwire.js
@@ -1,69 +1,97 @@
 import React from "react";
+//import ReactDOM from "react-dom";
+//import ReactHtmlParser from "react-html-parser";
+//import reactElementToJSXString from "react-element-to-jsx-string";
+//import ReactDOMServer from "react-dom/server";
 import ExecutionEnvironment from "exenv";
+
+/*class Forward extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div ref={this.props.fwdtwe} />;
+  }
+}*/
+
 class Cable extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { limit: [], cache: null };
+    this.state = { limit: [], cache: null, mountsCount: 0, cacheStyle: "" };
     this.page = React.createRef();
+    this.fwdtwe = React.createRef();
   }
   componentDidMount() {
     if (ExecutionEnvironment.canUseDOM) {
-      this.setState({ go: true });
+      this.setState({ go: true }, () => this.checkIfBetween());
     }
   }
   componentDidUpdate = (prevProps) => {
     if (this.state.go && this.props.scrolling !== prevProps.scrolling) {
-      const { cache } = this.state;
-      const { scrollTopAndHeight, scrollTop, girth, timeout } = this.props;
-      var girt = girth ? girth : 1000;
-      var timeou = timeout ? timeout : 1500;
-      var continuee = this.props.fwd.current;
-      if (!continuee && !cache) return;
-      clearTimeout(this.setset);
-      this.setset = setTimeout(() => {
-        var page = this.page.current;
-        var between =
-          page.offsetTop - scrollTop > Number(`-${girt}`) &&
-          scrollTopAndHeight - page.offsetTop > Number(`-${girt}`);
-        if (!continuee) continuee = cache;
-        this.setState({ cache: continuee, between }, () => {
-          if (!between && continuee) {
-            //while (page.firstChild) {
-            //page.removeChild(continuee);
-            continuee.remove();
-            //while (page.firstChild) {
-            //let onClick =
-            //"ontouchstart" in continuee ? "touchstart" : "onclick";
-            //if (continuee[onClick]) continuee.remove(); //touchevent
-            //continuee.click();
-            //}
-            return;
-          }
-          //if (!between && continuee) return continuee.remove();
-          const children = [...page.children];
-          if (
-            children.length === 0 ||
-            children[children.length - 1] !== this.state.cache
-          )
-            page.appendChild(this.state.cache);
-        });
-      }, timeou);
+      this.checkIfBetween();
     }
   };
   componentWillUnmount = () => {
     clearTimeout(this.setset);
   };
+  checkIfBetween = () => {
+    const { cache } = this.state;
+    const { scrollTopAndHeight, scrollTop, girth, timeout } = this.props;
+    var girt = girth ? girth : 1500;
+    var timeou = timeout ? timeout : 1500;
+    clearTimeout(this.setset);
+    this.setset = setTimeout(() => {
+      var page = this.page.current;
+      var between =
+        page.offsetTop - scrollTop > Number(`-${girt}`) &&
+        scrollTopAndHeight - page.offsetTop > Number(`-${girt}`);
+      if (between) {
+        !this.state.mount && this.setState({ mount: between }, () => {});
+      } else {
+        var continuee = this.props.fwd.current;
+        if (!continuee && !cache) return;
+        this.setState(
+          {
+            cache: cache ? cache : continuee.outerHTML,
+            cacheStyle: JSON.parse(
+              (cache ? cache : continuee.outerHTML)
+                .split(`style="`)[1]
+                .split(`"`)[0]
+                .replaceAll(";", `",`)
+                .replaceAll(": ", `: "`)
+            )
+          },
+          () => {
+            if (!between) {
+              return (page.innerHTML = "");
+            } else {
+              const children = [...page.children];
+              if (
+                continuee &&
+                (children.length === 0 ||
+                  !children.find((x) => x === this.state.cache))
+              ) {
+                console.log("replenishing, new scroll");
+                return (page.innerHTML = this.state.cache);
+              }
+            }
+          }
+        );
+      }
+    }, timeou);
+  };
   render() {
-    const { between, continuee } = this.state;
+    const { mount, cacheStyle } = this.state;
     const { src, float, title, img } = this.props;
     //const limited = limit.find((x) => x === Object.keys(this.props.fwd));
     const onError = (e) => {
       //this.props.fwd.current.remove();
       this.props.onError(e);
-    };
+    }; //ternaries remove the node and element; display removes the element, but not the node
     return (
-      <div ref={this.page}>
-        {src === "" ? (
+      <div ref={this.page} style={{ cacheStyle }}>
+        {!mount || src === "" ? (
           <span style={{ border: "1px gray solid" }}>{title}</span>
         ) : img ? (
           <img
@@ -74,8 +102,7 @@ class Cable extends React.Component {
               float,
               width: "200px",
               border: 0,
-              ...this.props.style,
-              display: continuee && !between ? "none" : ""
+              ...this.props.style
             }}
             ref={this.props.fwd}
             src={src}
@@ -89,8 +116,7 @@ class Cable extends React.Component {
               float,
               width: "200px",
               border: 0,
-              ...this.props.style,
-              display: continuee && !between ? "none" : ""
+              ...this.props.style
             }}
             ref={this.props.fwd}
             src={src}
